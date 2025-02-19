c31618715dd05a4ce22dc7da10734ba8fd742ab5
Raul <rpucholgimenez@gmail.com>
2025-02-19 17:24:17 +0100

    commit

--- resources/js/views/home/index.vue
+++ resources/js/views/home/index.vue
@@ -2,7 +2,10 @@
     <section id="banner">
         <h1>DUNKERQUE AIRSOFT CAMP</h1>
         <h2>La «Operación Dinamo» consistió en la defensa y la evacuación de fuerzas británicas, francesas y belgas por mar en Dunkerque (Francia).</h2>
-        <button class="normal-button">Reservar partida</button>
+        <div>
+            <button class="normal-button">Reservar partida publica Sabado 1</button>
+            <button class="normal-button">Reservar partida publica Domingo 2</button>
+        </div>
     </section>
     <section id="sobrenosotros" class="container">
         <div class="sobrenosotros-left">
@@ -46,9 +49,9 @@
             </div>
         </div>
     </section>
-    <section id="tickets" >
+    <section id="tickets">
     <h2>Proximas partidas</h2>
-        <div class="ticketbody">
+        <div class="ticketbody container" >
             <div class="ticketcaja">
                 <div class="ticketconboton">
                     <div class="ticket cutout-image">
@@ -58,258 +61,71 @@
                         </div>
                         <p>8h - 13h</p>
                     </div>
-                    <router-link to="/" class="ticketbutton">></router-link>
+                    <router-link to="/" class="ticketbutton cutout-image1"><p>></p></router-link>
                 </div>
                 <div class="ticketconboton">
-                    <div class="ticket">
+                    <div class="ticket cutout-image">
                         <div>
                             <p>Sábado 1 de Febrero</p>
                             <p>Partida pública</p>
                         </div>
                         <p>8h - 13h</p>
                     </div>
-                    <router-link to="/" class="ticketbutton">></router-link>
+                    <router-link to="/" class="ticketbutton cutout-image1"><p>></p></router-link>
                 </div>
             </div>
             <div class="ticketcaja">
                 <div class="ticketconboton">
-                    <div class="ticket">
+                    <div class="ticket cutout-image">
                         <div>
                             <p>Sábado 1 de Febrero</p>
                             <p>Partida pública</p>
                         </div>
                         <p>8h - 13h</p>
                     </div>
-                    <router-link to="/" class="ticketbutton">></router-link>
+                    <router-link to="/" class="ticketbutton cutout-image1"><p>></p></router-link>
                 </div>
                 <div class="ticketconboton">
-                    <div class="ticket">
+                    <div class="ticket cutout-image">
                         <div>
                             <p>Sábado 1 de Febrero</p>
                             <p>Partida pública</p>
                         </div>
                         <p>8h - 13h</p>
                     </div>
-                    <router-link to="/" class="ticketbutton">></router-link>
+                    <router-link to="/" class="ticketbutton cutout-image1"><p>></p></router-link>
                 </div>
 
             </div>
 
         </div>
     </section>
-    <section>
+    <section id="encontrarnos" class="container">
+        <h2>Como encontrarnos</h2>
+        <div class="mapsiconoscaja">
+            <div class="mapsiconos"><a href="https://maps.app.goo.gl/UJEW8JCeMrCVRszE8" target="_blank" ><img :src="'./images/googlemaps.svg'" alt="" height="40px"></a><a href="https://ul.waze.com/ul?place=ChIJbe--d3uJpBIRzkCD3RVVFok&ll=41.48944500%2C1.85023020&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location" target="_blank"  ><img :src="'./images/waze.svg'" alt="" height="40px"></a></div>
+        </div>
+        <div class="maps">
+            <div class="contactos">
+                <div>
+                    <h3><u>Nuestros contactos</u></h3>
+                    <p><img :src="'./images/telefono.svg'" alt="" height="24px"> Robert: +34 622 42 13 25</p>
+                    <p><img :src="'./images/telefono.svg'" alt="" height="24px"> Germán: +34 616 80 30 96</p>
+                    <p><img :src="'./images/telefono.svg'" alt="" height="24px"> Jose (alquileres): +34 611 07 22 55</p>
+                    <p><img :src="'./images/ubicacion.svg'" alt="" height="28px"> Masia Bach, 08635 Sant Esteve Sesrovires, Barcelona</p>
+                    <p><img :src="'./images/email.svg'" alt="" height="24px"> dunkerque.airsoft.camp@gmail.com</p>
+                    <a class="facebook" href="https://www.facebook.com/p/Dunkerque-Airsoft-Camp-100064113212080/?locale=ca_ES" ><img :src="'./images/facebook.svg'" alt="" height="35px"></a>
+                    <a href="https://www.instagram.com/dunkerque.airsoft.camp/" ><img :src="'./images/instagram.svg'" alt="" height="35px"></a>
+                </div>
+            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2186.6496725878847!2d1.8500931773698173!3d41.48928309397323!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4897b77beef6d%3A0x89165515dd8340ce!2sDunkerque%20Airsoft%20Camp!5e1!3m2!1ses!2ses!4v1739816091320!5m2!1ses!2ses" width="800" height="600" style="border:0;" allowfullscreen="false" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </section>
+    <section id="colaboradores">
+    <h2>Colaboradores</h2>
 
     </section>
 </template>
-<!--
-<template>
-    <div class="demo" id="edit-demo">
-        <div class="viewport" @click="select(null)" @mousedown.capture="blockEvents" @wheel.capture="blockEvents">
-            <screen ref="screen">
-                <g v-for="edge in graph.edges" @click.stop="select(edge)" :key="edge.id">
-                    <edge :class="selection && selection.id === edge.id && 'selected'"
-                          :data="edge"
-                          :nodes="graph.nodes">
-                    </edge>
-                </g>
-                <g v-for="node in graph.nodes" :key="node.id">
-                    <node :data="node" ref="node" :class="isSelected(node) && 'selected'" :textSelect="node.textSelect" :useDrag="node.useDrag">
-                        <div v-html="node.html" @click.stop="select(node)">
-                        </div>
-                    </node>
-                </g>
-            </screen>
-        </div>
-        <div class="sidebar">
-            <codemirror v-model="editText" :options="{
-          mode: 'text/javascript',
-          theme: 'default',
-          lineWrapping: true,
-          scrollbarStyle: null,
-          styleActiveLine: true,
-          line: true,
-        }"
-                        style="font-size: 13.3333px; font-family: monospace; -webkit-text-size-adjust: 100%; height: 100%"
-            ></codemirror>
-        </div>
-    </div>
-</template>
-
-<script>
-import Screen from '../components/Screen.vue'
-import Node from '../components/Node.vue'
-import Edge from '../components/Edge.vue'
-import graph from '../graph'
-import pretty from 'pretty'
-import stringify from 'javascript-stringify'
-import { Codemirror } from 'vue-codemirror'
-// import 'codemirror/mode/javascript/javascript.js'
-// import 'codemirror/lib/codemirror.css'
-
-export default {
-    components: {
-        Screen,
-        Node,
-        Edge,
-        Codemirror
-    },
-    data() {
-        return {
-            graph: new graph(),
-            selection: null,
-            editText: '/* click on a node or edge to start editing */',
-        }
-    },
-    methods: {
-        select (obj) {
-            this.selection = obj
-            if (!this.selection) {
-                this.editText = '/* click on a node or edge to start editing */'
-                return
-            }
-            const editText = { ...obj }
-            delete editText.pathd
-            if (editText.html) {
-                editText.html = "\n" + pretty(editText.html) + "\n"
-            }
-            this.editText = stringify(editText, null, 2)
-                .replace(/\\n/g, "\n")
-                .replace(/html: '([^]*)\s'/g, 'html: `$1\n`')
-        },
-        applyChanges () {
-            if (!this.selection) {
-                return
-            }
-            try {
-                const edit = eval('('+this.editText+')')
-                Object.assign(this.selection, edit)
-                this.$nextTick(() => {
-                    this.$refs.node.forEach(node => {
-                        node.fitContent()
-                    })
-                })
-            } catch (_) {
-                console.log('TODO invalid code')
-            }
-        },
-        isSelected (node) {
-            return this.selection
-                && this.selection.id === node.id
-        },
-        /**
-         * HACKS
-         * support shortcut .no-drag and .no-wheel classes
-         * to disable dragging and mouse-wheel behavior from editable html
-         */
-        blockEvents (e) {
-            const path = e.path || e.composedPath?.();
-            if (path?.find(el => el.classList?.contains('no-drag'))) { // @mousedown
-                const pz = this.$refs.screen.panzoom
-                pz.options.preventMouseEventsDefault = false // enable default events (text select, input click)
-                document.addEventListener('mouseup', () => {
-                    pz.options.preventMouseEventsDefault = true
-                }, { once: true })
-                e.stopPropagation() // disable node drag
-            }
-            if (path?.find(el => el.classList?.contains('no-wheel'))) { // @wheel
-                e.stopPropagation() // disable wheel zoom
-            }
-        },
-    },
-    mounted () {
-        this.graph.createNode({
-            id: 'a',
-            html: '<h5>A</h5>'
-        })
-        this.graph.createNode({
-            id: 'b',
-            x: 200,
-            y: 200,
-            textSelect: false,
-            useDrag: true,
-            html:
-                `<div><h4>B</h4><p>Subtitle</p><button>Yo</button></div>`
-        })
-        this.graph.createNode({
-            id: 'c',
-            x: -100,
-            y: 150,
-            textSelect: false,
-            useDrag: true,
-            html: `<div> <h4>okay</h4> <textarea type="text" class="no-drag">Some text here</textarea><br/><select class="no-drag" name="cars" id="cars"><option value="volvo">Volvo</option><option value="saab">Saab</option><option value="mercedes">Mercedes</option><option value="audi">Audi</option></select></div>`
-        })
-        this.graph.createNode({
-            id: 'd',
-            x: 340,
-            textSelect: false,
-            useDrag: true,
-            html: `<div>Okay</div>`
-        })
-        this.graph.createEdge({
-            id: 'a:b',
-            from: 'a',
-            to: 'b',
-            toAnchor: { x: '50%', y: '50%', snap: 'rect' },
-            type: 'smooth'
-        })
-        this.graph.createEdge({ id: 'c:d', from: 'c', to: 'd', type: 'smooth' })
-        this.$nextTick(() => {
-            this.$refs.screen.zoomNodes(this.graph.nodes, {scale: 1})
-        })
-    },
-    watch: {
-        editText: 'applyChanges',
-    },
-}
-</script>
-
-<style>
-#edit-demo .CodeMirror {
-    width: 100%;
-    height: 500px;
-    margin: 0;
-    overflow: hidden;
-    position: relative;
-    background-color: #f1f1f1;
-    border: 1px solid #f1f1f1;
-}
-#edit-demo .node .background {
-    /* background-color: #ccc; */
-}
-
-#edit-demo .node .content > div {
-    padding: 25px;
-}
-
-#edit-demo .node .content h4,h5,p {
-    margin: 0
-}
-
-#edit-demo .node:hover .background {
-    background-color: rgb(90 200 90);
-}
-
-#edit-demo .node.selected .content {
-    background-color: rgba(100, 200, 100, 1);
-    box-shadow: 0px 0px 0px 2px #333;
-}
-
-#edit-demo .node .content {
-    cursor: pointer;
-}
-
-#edit-demo .edge {
-    cursor: pointer;
-}
-#edit-demo .edge:hover {
-    /* stroke-width: 4 */
-    stroke: rgb(90 200 90)
-}
-#edit-demo .edge.selected {
-    /* stroke-width: 4 */
-    stroke: #333
-}
-</style>
--->
 <script setup lang="ts">
+
 </script>
