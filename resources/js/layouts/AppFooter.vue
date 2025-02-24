baad11562d37fb264327edb4cbd8cd08384f7d1d
Raul <rpucholgimenez@gmail.com>
2025-02-24 19:19:32 +0100

    commit footer

--- resources/js/layouts/AppFooter.vue
+++ resources/js/layouts/AppFooter.vue
@@ -12,25 +12,25 @@ const logoUrl = computed(() => {
 <template>
     <div class="layout-footer container">
         <div class="footerup">
-            <img :src="'./images/logo.svg'" alt="airsoftlogo">
-            <div>
+            <img :src="'./images/logo.svg'" alt="airsoftlogo" width="268px">
+            <div class="footercontactoyubicacion">
                 <h3>Contacto</h3>
                 <p> Robert: +34 622 42 13 25</p>
                 <p> Germán: +34 616 80 30 96</p>
                 <p> Jose (alquileres): +34 611 07 22 55</p>
                 <p> dunkerque.airsoft.camp@gmail.com</p>
             </div>
-            <div>
+            <div class="footercontactoyubicacion">
                 <h3>Ubicación</h3>
                 <p> Masia Bach, 08635 Sant Esteve Sesrovires, Barcelona</p>
+                <h3>Red social</h3>
+                <a class="facebook" href="https://www.facebook.com/p/Dunkerque-Airsoft-Camp-100064113212080/?locale=ca_ES" ><img class="icons" :src="'./images/facebook-green.svg'" alt="" height="45px"></a>
+                <a href="https://www.instagram.com/dunkerque.airsoft.camp/" ><img class="icons" :src="'./images/instagram-green.svg'" alt="" height="45px"></a>
             </div>
-            <div>
-                <a class="facebook" href="https://www.facebook.com/p/Dunkerque-Airsoft-Camp-100064113212080/?locale=ca_ES" ><img class="icons" :src="'./images/facebook.svg'" alt="" height="35px"></a>
-                <a href="https://www.instagram.com/dunkerque.airsoft.camp/" ><img class="icons" :src="'./images/instagram.svg'" alt="" height="35px"></a>
-            </div>
+
         </div>
         <div class="footerbottom">
-
+            <p>Dunkerque © {{ new Date().getFullYear() }}. Todos los derechos reservados. <img class="icons" :src="'./images/CIRCULO.svg'" alt="" height="4px">   <router-link to="/">Aviso legal</router-link> | <router-link to="/">Política de privacidad</router-link> |  <router-link to="/">Política de cookies</router-link></p>
         </div>
     </div>
 </template>
