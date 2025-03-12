582f2ff7a8132f9113a69b7534361c9b1982a900
Marcos <marcosfuentes@ibf.cat>
2025-03-12 17:15:16 +0100

    calendario reservas partidas

--- resources/js/views/posts/index.vue
+++ resources/js/views/posts/index.vue
@@ -1,55 +1,16 @@
 <template>
     <div class="container">
         <div class="nav-avoid">
-            <h1>Enero 2021</h1>
-            <ol class="booking-ol">
-                <li class="day-name">Lun</li>
-                <li class="day-name">Mar</li>
-                <li class="day-name">Mié</li>
-                <li class="day-name">Jue</li>
-                <li class="day-name">Vie</li>
-                <li class="day-name">Sáb</li>
-                <li class="day-name">Dom</li>
-
-                <li class="first-day">1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-                <li>6</li>
-                <li>7</li>
-                <li>8</li>
-                <li>9</li>
-                <li>10</li>
-                <li>11</li>
-                <li>12</li>
-                <li>13</li>
-                <li>14</li>
-                <li>15</li>
-                <li>16</li>
-                <li>17</li>
-                <li>18</li>
-                <li>19</li>
-                <li>20</li>
-                <li>21</li>
-                <li>22</li>
-                <li>23</li>
-                <li>24</li>
-                <li>25</li>
-                <li>26</li>
-                <li>27</li>
-                <li>28</li>
-                <li>29</li>
-                <li>30</li>
-                <li>31</li>
-            </ol>
+            <h1>{{ $t('booking') }}</h1>
         </div>
+        <selfCalendar/>
     </div>
 </template>
 
 <script setup>
 import axios from 'axios';
-import {ref, onMounted} from 'vue'
+import {ref, onMounted} from 'vue';
+import selfCalendar from './../../components/Calendar.vue';
 
 const posts = ref();
 
@@ -68,4 +29,5 @@ onMounted(() => {
         posts.value = data;
     })
 })
+
 </script>
