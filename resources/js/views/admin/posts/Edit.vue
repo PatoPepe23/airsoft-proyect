6a389a3ce823c4aab00932196240f7d6c155c93d
Raul <rpucholgimenez@gmail.com>
2025-04-22 22:41:03 +0200

    qr scanner

--- resources/js/views/admin/posts/Edit.vue
+++ resources/js/views/admin/posts/Edit.vue
@@ -5,6 +5,7 @@
                 <div class="flex flex-wrap items-center justify-between gap-2">
                     <span class="text-xl font-bold">Jugadores</span>
                     <Button icon="pi pi-refresh" rounded raised />
+
                 </div>
             </template>
             <Column field="DNI" sortable header="DNI"></Column>
@@ -13,6 +14,15 @@
             <Column field="mail" header="Correo"></Column>
             <Column field="phone" header="Telefono"></Column>
             <Column field="status" header="Status"></Column>
+            <button @click="openCamera" class="btn btn-primary">Abrir Cámara</button><button @click="closeCamera" class="btn btn-danger">Cerrar Cámara</button>
+            <div v-if="isCameraOpen" class="camera-container">
+                <QrcodeStream
+                    v-if="isCameraOpen"
+                    @decode="onDecode" @init="onInit" @error="onError" :constraints="{ video: { facingMode: 'environment' } }"
+                />
+
+            </div>
+
             <column header="Opciones">
                 <template #body="player">
                     <button @click.prevent.stop="playerCheck(player.data.DNI, route.params.id)"
@@ -27,9 +37,37 @@
 import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import usePosts from '@/composables/posts';
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'; // Importa el lector QR
 const {playerCheck, getPost, playersData } = usePosts(); // Correctly import playersData
 
 const route = useRoute();
+const isCameraOpen = ref(false); // Estado para manejar la cámara
+const scannedCode = ref(null); // Almacena el código escaneado
+
+const openCamera = () => {
+    isCameraOpen.value = true; // Activa el escáner de QR
+};
+
+const closeCamera = () => {
+    isCameraOpen.value = false; // Cierra el escáner de QR
+};
+
+const onDecode = (decodedString) => {
+    console.log('Código QR escaneado:', decodedString);
+
+    // Llamar directamente a playerCheck pasando el DNI escaneado
+    playerCheck(decodedString, route.params.id);
+
+    isCameraOpen.value = false; // Cerrar el escáner después del escaneo
+};
+
+const onInit = (initStatus) => {
+    console.log('Estado de inicialización del escáner:', initStatus);
+};
+
+const onError = (error) => {
+    console.error('Error al abrir la cámara:', error);
+};
 
 const loadPlayerData = async () => {
     try {
