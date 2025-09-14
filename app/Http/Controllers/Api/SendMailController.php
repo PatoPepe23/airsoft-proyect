a4fede3a48b1dd2232228717d14f63065de51685
Cab <rpucholgimenez@gmail.com>
2025-09-14 02:23:30 +0200

    reservaworking?

--- app/Http/Controllers/Api/SendMailController.php
+++ app/Http/Controllers/Api/SendMailController.php
@@ -33,16 +33,23 @@ class SendMailController extends Controller
     {
         $data = $request->all();
 
-        // Si el usuario está autenticado, usamos su QR almacenado
-        if (auth()->check() && auth()->user()->qrimg) {
-            // Ruta al QR ya existente
+        $reservadoDni = $request->DNI;
+
+        // Si la reserva es para el mismo usuario autenticado
+        if (auth()->check() && auth()->user()->DNI === $reservadoDni) {
+            // Usamos su QR ya almacenado
             $qr = public_path("images/QR/" . auth()->user()->qrimg);
-            Mail::to($data['email'])->send(new BookingConfirmation($data, $qr));
         } else {
-            // Si no está autenticado o no tiene QR, generamos uno nuevo con su DNI
-            $qrPath = $this->qrService->generate($request->DNI);
+            // Generamos un nuevo QR para la persona de esta reserva
+            $qrPath = $this->qrService->generate($reservadoDni);
             $qr = $qrPath['path'];
-            Mail::to($data['email'])->send(new BookingConfirmation($data, $qr));
+        }
+
+        // Enviamos el correo de confirmación con los datos correctos
+        Mail::to($data['email'])->send(new BookingConfirmation($data, $qr));
+
+        // Si el QR fue generado temporalmente, lo borramos
+        if (! (auth()->check() && auth()->user()->DNI === $reservadoDni)) {
             if (file_exists($qr)) {
                 unlink($qr);
             }
